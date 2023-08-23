3c89b8e91dc92366d2c836f1eabbc92b9326d8fd
Daniel Ntege <danientege785@gmail.com>
2023-08-23 12:20:45 +0300

    data bank

---
+++ resourses/databank.js
@@ -0,0 +1,17 @@
+const automatedScenarios = [
+    {
+      keywords: ['current air quality', 'temperature', 'weather'],
+      response: 'Automated AI response for weather-related queries.',
+    },
+    {
+      keywords: ['hello', 'hi', 'hey'],
+      response: 'Hello! How can I assist you today?',
+    },
+    {
+      keywords: ['forgot password', 'reset password','hello'],
+      response: 'To reset your password, please follow these steps: [click on the Forgot password link,].',
+    },
+    // Add more automated scenarios as needed
+];
+
+export default automatedScenarios;
